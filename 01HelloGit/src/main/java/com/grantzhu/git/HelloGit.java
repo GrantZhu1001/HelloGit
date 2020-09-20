3f58432aba84e2f0a3818e17774cdff8cfe6bf30
Rlyi <a1065135230@gmail.com>
2020-09-20 15:39:54 +0800

    添加文件

---
+++ 01HelloGit/src/main/java/com/grantzhu/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.grantzhu.git;
+
+/**
+ * author GrantZhu
+ * create 2020-09-20 15:29
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
